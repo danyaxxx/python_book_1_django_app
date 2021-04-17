6f7a24e17235819df5568111692a8849a45f4987
Daniil <danya.trishchanovskiy.01@mail.ru>
2021-04-17 17:45:29 +0300

    Функция представления new_topic(). Шаблон new_topic. Создание ссылки на страницу new_topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from .models import Topic
+from .forms import TopicForm
 
 
 def index(request):
@@ -18,3 +20,19 @@ def topic(request, topic_id):
     entires = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entires}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Определяет новую тему"""
+    if request.method != 'POST':
+        # Данные не отправлялись; создается пустая форма.
+        form = TopicForm()
+    else:
+        # Оправлены данные POST; обработать данные.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Вывести пустую или недействительную форму.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
