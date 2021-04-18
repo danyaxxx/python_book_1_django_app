c83d3b25c1688b25dd9e2d34d228633f77cf0e57
Daniil <danya.trishchanovskiy.01@mail.ru>
2021-04-18 19:05:46 +0300

    Страница входа. Шаблон login. Создание ссылки на страницу входа.

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,8 @@
-from django.contrib import admin
+"""Определяет схемы URL для пользователей"""
 from django.urls import path, include
 
+app_name = 'users'
 urlpatterns = [
+    # Включить URL авторизации по умолчанию.
+    path('', include('django.contrib.auth.urls')),
 ]
