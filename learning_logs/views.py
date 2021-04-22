708a3bbac1020d4114ed480cf990464f01822572
Daniil <danya.trishchanovskiy.01@mail.ru>
2021-04-22 20:03:56 +0300

    Ограничение доступа в Learning Log.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -9,6 +9,7 @@ def index(request):
     """Домашняя страница приложения Learning Log"""
     return render(request, 'learning_logs/index.html')
 
+
 @login_required
 def topics(request):
     """Выводит список тем."""
@@ -16,6 +17,8 @@ def topics(request):
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+
+@login_required
 def topic(request, topic_id):
     """Выводит одну тему и все ее записи."""
     topic = Topic.objects.get(id=topic_id)
@@ -23,6 +26,8 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entires}
     return render(request, 'learning_logs/topic.html', context)
 
+
+@login_required
 def new_topic(request):
     """Определяет новую тему"""
     if request.method != 'POST':
@@ -39,6 +44,8 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+
+@login_required
 def new_entry(request, topic_id):
     """Добавляет новую запсь по конкрентной теме."""
     topic = Topic.objects.get(id=topic_id)
@@ -58,6 +65,8 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+
+@login_required
 def edit_entry(request, entry_id):
     """Редактирует существующую запись."""
     entry = Entry.objects.get(id=entry_id)
