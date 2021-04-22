bf17028bb834fa8fe1f7fdbb34d74da26a34e364
Daniil <danya.trishchanovskiy.01@mail.ru>
2021-04-22 19:54:01 +0300

    Редактирование данных. Ограничение доступа с использованием @login_require. Ограничение доступа к страницам тем.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryFrom
@@ -8,6 +9,7 @@ def index(request):
     """Домашняя страница приложения Learning Log"""
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
     """Выводит список тем."""
     topics = Topic.objects.order_by('date_added')
